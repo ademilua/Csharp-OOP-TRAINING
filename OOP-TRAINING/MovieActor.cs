74675adb41f85dd5b98a7aed156b226eb05dddf5
Tolulopequix <tolulope@iamquix.com>
2021-03-22 15:11:20 +0200

    refactor code

--- OOP-TRAINING/MovieActor.cs
+++ OOP-TRAINING/MovieActor.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace OOP_TRAINING
 {
-    public class MovieActor : Actor, IDancer
+    public class MovieActor : Actor
     {
         public MovieActor(string firstName, string lastName) : base(firstName, lastName)
         {
